b0ce1f045a6999c0830208375bb63f61c9bc4b88
Vasco Santos <vasco.santos@moxy.studio>
2017-05-22 10:54:21 +0100

    set emoji

--- react-project/src/pages/home/register-form/RegisterForm.js
+++ react-project/src/pages/home/register-form/RegisterForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Picker from 'react-emojipicker'
 import './RegisterForm.css';
 
 class RegisterForm extends Component {
@@ -7,35 +8,46 @@ class RegisterForm extends Component {
     this.state = {
       username: undefined,
       emoji: undefined,
+      selectEmoji: false,
     };
     this._handleSubmit = this._handleSubmit.bind(this);
     this._handleUsernameChange = this._handleUsernameChange.bind(this);
+    this._handleEmojiChoose = this._handleEmojiChoose.bind(this);
     this._handleEmojiChange = this._handleEmojiChange.bind(this);
   }
 
   render() {
+    const { username, emoji, selectEmoji } = this.state;
     return (
       <form className="home-register"
         onSubmit={ this._handleSubmit }>
-        <div className="home-register-inputs">
-
-          <div className="home-register-input">
+        <div className="home-register-input">
+          <div className="home-register-input-cel">
             <div className="home-register-input-label">name: </div>
             <input type="text" className="home-register-input-value"
-              value={ this.state.username } onChange={ this._handleUsernameChange } />
+              value={ username } onChange={ this._handleUsernameChange } />
           </div>
-
-          <div className="home-register-input">
-            <div className="home-register-input-label">emoji: </div>
-            <select className="home-register-input-value"
-              value={ this.state.value } onChange={ this._handleEmojiChange }>
-              <option value="grapefruit">Grapefruit</option>
-              <option value="lime">Lime</option>
-              <option value="coconut">Coconut</option>
-              <option value="mango">Mango</option>
-            </select>
+          <div className="home-register-input-cel">
+            <div className="home-register-input-emoji-label">emoji: </div>
+            <div className="home-register-input-emoji-choose">
+              <button className="home-register-input-emoji-choose-cta"
+                onClick={ this._handleEmojiChoose }>
+                pick
+              </button>
+              <Picker modal visible={ selectEmoji }
+                onEmojiSelected={ this._handleEmojiChange } />
+            </div>
+            <div className="home-register-input-emoji-value">
+              {
+                emoji ?
+                  emoji.unicode
+                :
+                  null
+              }
+            </div>
           </div>
         </div>
+
         <input className="home-register-submit" type="submit" value="Get in" />
       </form>
     );
@@ -49,9 +61,14 @@ class RegisterForm extends Component {
     console.log('change', event);
   }
 
-  _handleEmojiChange(event) {
-    alert('Your favorite flavor is: ' + this.state.value);
-    event.preventDefault();
+  _handleEmojiChoose(e) {
+    this.setState({ selectEmoji: true });
+    e.preventDefault();
+  }
+
+  _handleEmojiChange(data) {
+    console.log(data);
+    this.setState({ selectEmoji: false, emoji: data });
   }
 }
 
