67da30aaa691a82694b590a03880b171e8bf7a17
carsy <jlageb@gmail.com>
2017-05-22 18:45:01 +0100

    add api server

---
+++ api/src/index.js
@@ -0,0 +1,51 @@
+import { Server } from 'http';
+import express from 'express';
+import socketIO from 'socket.io';
+
+const app = express();
+const server = Server(app);
+const io = socketIO(server);
+
+const logger = {
+    debug: console.log.bind(console, ''),
+    event: (event, message) => console.log(`[${event}]` + (message ? `: ${message}` : '')),
+    info: console.info.bind(console, '# ')
+};
+
+io.on('connection', socket => {
+    let user;
+
+    logger.event('connection');
+
+    socket.on('userInfo', data => {
+        logger.event('userInfo', `${data.name} ${data.emoji}`);
+
+        user = data;
+        socket.broadcast.emit('userConnected', user);
+    });
+
+    socket.on('message', data => {
+        const message = { text: data.text, user };
+
+        logger.event('message', `${data.user.emoji}  ${data.user.name}: ${data.text}`);
+
+        socket.broadcast.emit('message', data);
+    });
+
+    socket.on('disconnect', () => {
+        if (!user) {
+            logger.event('user disconnect');
+
+            return;
+        }
+
+        logger.event('disconnect', `${user.name} ${user.emoji}`);
+
+        socket.broadcast.emit('userDisconnected', user);
+    });
+});
+
+
+server.listen(3000, () => {
+    logger.info('server started');
+});
